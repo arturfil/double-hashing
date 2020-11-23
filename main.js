ebdfa24942272e11c88f2d677c0c10d3ea3632a7
arturfil <arturfil@hotmail.com>
2020-11-23 13:48:13 -0500

    made changes to the code

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
-var TABLE_SIZE = 13 ;
+const TABLE_SIZE = 16 ;
 
-var bucketcolor = new Array( 
+const bucketcolor = [
 	'#ff8',
 	'#dca',
 	'#f88',
@@ -18,82 +18,67 @@ var bucketcolor = new Array(
 	'#88f',
 	'#8ff',
 	'#8f8'
-	);
+];
 
 function setElementValue(elementId, value) {
-	if ( document.getElementById(elementId).innerText!=undefined ) {
+	if ( document.getElementById(elementId).innerText!=undefined ) 
 		document.getElementById(elementId).innerText = value ;
-	} else {
+	else
 		document.getElementById(elementId).textContent = value ;
-	}
 }
 
-function setCellColor(cell, color) {
-	document.getElementById(cell).style.background = color ;
-}
+const setCellColor = (cell, color) => document.getElementById(cell).style.background = color ;
 
-function makeBucketHTMLString(i) {
+const makeBucketHTMLString = (i) => {
 	return '<div class="bucketbox" id="bb_'+i+'" style="background: '+bucketcolor[i]+';">'
 		+'<div id="bucket_'+i+'" class="buckettext"><'
 		+ '/div><div class="bucketnumber">'+i+'<'+'/div><'+'/div>' ;
 }
 
 
-function initElement() {
-	var el = document.getElementById("hashtablecontainer") ;
-	var i ;
-	for (i=0;i<TABLE_SIZE;i++ ) {
+const initElement = () => {
+	let el = document.getElementById("hashtablecontainer") ;
+	for (let i=0;i<TABLE_SIZE;i++ ) {
 		el.innerHTML = el.innerHTML + makeBucketHTMLString(i) ;
 	}
 }
  
 function setElementValue(elementId, value) {
-	if ( document.getElementById(elementId).innerText!=undefined ) {
+	if ( document.getElementById(elementId).innerText!=undefined ) 
 		document.getElementById(elementId).innerText = value ;
-	} else {
+	else 
 		document.getElementById(elementId).textContent = value ;
-	}
 }
 
 function getElementValue(elementId) {
-	if ( document.getElementById(elementId).innerText!=undefined ) {
+	if ( document.getElementById(elementId).innerText!=undefined ) 
 		return document.getElementById(elementId).innerText ;
-	} else {
+	else 
 		return document.getElementById(elementId).textContent ;
-	}
 }
 
-function getFromBucket(b) {
-// check for IE or Gecko. This is Gecko only:
-	return getElementValue('bucket_'+b) ;
-}
+const getFromBucket = (b) => getElementValue('bucket_'+b) ;
 
-function putToBucket(b,t) {
-// check for IE or Gecko. This is Gecko only:
+const putToBucket = (b,t) => {
 	setElementValue('bucket_'+b,t) ;
 	document.getElementById('bucket_'+b).style.color="#0a0" ;
 }
 
-function makeBucketsBlack() {
-	var i ;
-	for ( i=0;i<TABLE_SIZE;i++ )
+const makeBucketsBlack = () => {
+	for (let i=0;i<TABLE_SIZE;i++ )
 		document.getElementById('bucket_'+i).style.color="#000" ;
 }
 
-function colorBucketTouched(i) {
-	document.getElementById('bucket_'+i).style.color="#a00" ;
-}
+const colorBucketTouched = (i) => document.getElementById('bucket_'+i).style.color="#a00" ;
 
-function clearInputValues() {
-	var i ;
-	for ( i=0;i<TABLE_SIZE;i++ ) {
+const clearInputValues = () => {
+	for (let i=0;i<TABLE_SIZE;i++ )
 		putToBucket(i,"") ;
-	}
 }
 
-function hashTableInsert(j) {
-	var i = 0 ;
-	var h = j%TABLE_SIZE ;
+const hashTableInsert = (j) => {
+	let i = 0 ;
+	let h = j%TABLE_SIZE ;
 	while ( getFromBucket(h) != "" ) {
 		colorBucketTouched(h) ;
 		h = (h+1)%TABLE_SIZE ;
@@ -104,12 +89,11 @@ function hashTableInsert(j) {
 }
 
 
-function getInputValue() {
-    var s = document.getElementById('hashin').value ;
-	var j = parseInt(s) ;
+const getInputValue = () => {
+    let s = document.getElementById('hashin').value ;
+	let j = parseInt(s) ;
 	makeBucketsBlack() ;
-	if ( !isNaN(j) ) {
+	if ( !isNaN(j) ) 
 		hashTableInsert(j) ;
-	}
 	document.getElementById('hashin').value="" ;
 }
